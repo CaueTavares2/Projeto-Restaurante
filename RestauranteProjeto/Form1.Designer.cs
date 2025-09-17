88691ac528aeddcdc008227c39ea4761ac44ffb0
Aluno <Aluno@A040-117438>
2025-09-17 16:31:02 -0300

    Adicionar arquivos de projeto.

---
+++ RestauranteProjeto/Form1.Designer.cs
@@ -0,0 +1,220 @@
+﻿
+namespace RestauranteProjeto
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pbMenu = new System.Windows.Forms.PictureBox();
+            this.pbReservar = new System.Windows.Forms.PictureBox();
+            this.pbReservas = new System.Windows.Forms.PictureBox();
+            this.pbPromocao = new System.Windows.Forms.PictureBox();
+            this.btnlogin = new System.Windows.Forms.Button();
+            this.pbOpcao1 = new System.Windows.Forms.PictureBox();
+            this.pbOpcao2 = new System.Windows.Forms.PictureBox();
+            this.pbOpcao3 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbMenu)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbReservar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbReservas)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbPromocao)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbOpcao1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbOpcao2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbOpcao3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::RestauranteProjeto.Properties.Resources.FundoOfertas;
+            this.pictureBox2.Location = new System.Drawing.Point(396, 243);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(177, 234);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::RestauranteProjeto.Properties.Resources.FundoOfertas;
+            this.pictureBox3.Location = new System.Drawing.Point(657, 243);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(179, 234);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 2;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::RestauranteProjeto.Properties.Resources.FundoOfertas;
+            this.pictureBox1.Location = new System.Drawing.Point(111, 243);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(176, 234);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pbMenu
+            // 
+            this.pbMenu.Image = global::RestauranteProjeto.Properties.Resources.Menu;
+            this.pbMenu.Location = new System.Drawing.Point(111, 169);
+            this.pbMenu.Name = "pbMenu";
+            this.pbMenu.Size = new System.Drawing.Size(148, 56);
+            this.pbMenu.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbMenu.TabIndex = 4;
+            this.pbMenu.TabStop = false;
+            this.pbMenu.Click += new System.EventHandler(this.pictureBox4_Click);
+            // 
+            // pbReservar
+            // 
+            this.pbReservar.Image = global::RestauranteProjeto.Properties.Resources.Reservar;
+            this.pbReservar.Location = new System.Drawing.Point(291, 173);
+            this.pbReservar.Name = "pbReservar";
+            this.pbReservar.Size = new System.Drawing.Size(146, 47);
+            this.pbReservar.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbReservar.TabIndex = 5;
+            this.pbReservar.TabStop = false;
+            // 
+            // pbReservas
+            // 
+            this.pbReservas.Image = global::RestauranteProjeto.Properties.Resources.MinhasReservas;
+            this.pbReservas.Location = new System.Drawing.Point(472, 172);
+            this.pbReservas.Name = "pbReservas";
+            this.pbReservas.Size = new System.Drawing.Size(154, 48);
+            this.pbReservas.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbReservas.TabIndex = 6;
+            this.pbReservas.TabStop = false;
+            // 
+            // pbPromocao
+            // 
+            this.pbPromocao.Image = global::RestauranteProjeto.Properties.Resources.Promoções;
+            this.pbPromocao.Location = new System.Drawing.Point(669, 169);
+            this.pbPromocao.Name = "pbPromocao";
+            this.pbPromocao.Size = new System.Drawing.Size(167, 51);
+            this.pbPromocao.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbPromocao.TabIndex = 7;
+            this.pbPromocao.TabStop = false;
+            // 
+            // btnlogin
+            // 
+            this.btnlogin.BackColor = System.Drawing.Color.Gold;
+            this.btnlogin.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnlogin.Location = new System.Drawing.Point(814, 65);
+            this.btnlogin.Name = "btnlogin";
+            this.btnlogin.Size = new System.Drawing.Size(103, 42);
+            this.btnlogin.TabIndex = 8;
+            this.btnlogin.Text = "LOGIN";
+            this.btnlogin.UseVisualStyleBackColor = false;
+            // 
+            // pbOpcao1
+            // 
+            this.pbOpcao1.BackColor = System.Drawing.Color.White;
+            this.pbOpcao1.Image = global::RestauranteProjeto.Properties.Resources.Opções;
+            this.pbOpcao1.Location = new System.Drawing.Point(133, 416);
+            this.pbOpcao1.Name = "pbOpcao1";
+            this.pbOpcao1.Size = new System.Drawing.Size(136, 50);
+            this.pbOpcao1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbOpcao1.TabIndex = 9;
+            this.pbOpcao1.TabStop = false;
+            this.pbOpcao1.Click += new System.EventHandler(this.pictureBox8_Click_1);
+            // 
+            // pbOpcao2
+            // 
+            this.pbOpcao2.BackColor = System.Drawing.Color.White;
+            this.pbOpcao2.Image = global::RestauranteProjeto.Properties.Resources.Opções;
+            this.pbOpcao2.Location = new System.Drawing.Point(417, 416);
+            this.pbOpcao2.Name = "pbOpcao2";
+            this.pbOpcao2.Size = new System.Drawing.Size(136, 50);
+            this.pbOpcao2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbOpcao2.TabIndex = 10;
+            this.pbOpcao2.TabStop = false;
+            // 
+            // pbOpcao3
+            // 
+            this.pbOpcao3.BackColor = System.Drawing.Color.White;
+            this.pbOpcao3.Image = global::RestauranteProjeto.Properties.Resources.Opções;
+            this.pbOpcao3.Location = new System.Drawing.Point(679, 416);
+            this.pbOpcao3.Name = "pbOpcao3";
+            this.pbOpcao3.Size = new System.Drawing.Size(136, 50);
+            this.pbOpcao3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbOpcao3.TabIndex = 11;
+            this.pbOpcao3.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(27)))), ((int)(((byte)(55)))));
+            this.BackgroundImage = global::RestauranteProjeto.Properties.Resources._1;
+            this.ClientSize = new System.Drawing.Size(962, 515);
+            this.Controls.Add(this.pbOpcao3);
+            this.Controls.Add(this.pbOpcao2);
+            this.Controls.Add(this.pbOpcao1);
+            this.Controls.Add(this.btnlogin);
+            this.Controls.Add(this.pbPromocao);
+            this.Controls.Add(this.pbReservas);
+            this.Controls.Add(this.pbReservar);
+            this.Controls.Add(this.pbMenu);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbMenu)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbReservar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbReservas)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbPromocao)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbOpcao1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbOpcao2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbOpcao3)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pbMenu;
+        private System.Windows.Forms.PictureBox pbReservar;
+        private System.Windows.Forms.PictureBox pbReservas;
+        private System.Windows.Forms.PictureBox pbPromocao;
+        private System.Windows.Forms.Button btnlogin;
+        private System.Windows.Forms.PictureBox pbOpcao1;
+        private System.Windows.Forms.PictureBox pbOpcao2;
+        private System.Windows.Forms.PictureBox pbOpcao3;
+    }
+}
+
